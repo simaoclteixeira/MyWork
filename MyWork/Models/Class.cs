b4fd9906b11577c3f6133ae88ad575fbd4433f08
simaoclteixeira <sclteixeira11@gmail.com>
2019-11-25 12:34:39 +0000

    Criação da classe funcionários

---
+++ MyWork/Models/Class.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWork.Models
+{
+    public class Funcionário
+    {
+        public int IDFuncionarios { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Numero { get; set; }
+
+
+    }
+}
