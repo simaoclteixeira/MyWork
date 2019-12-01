96d76dab42b24b80f67d8266f2253544ac921f76
simaoclteixeira <sclteixeira11@gmail.com>
2019-12-01 23:39:48 +0000

    Criação do modelo Departamentos.

---
+++ MyWork/Models/Departamentos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWork.Models
+{
+    public class Departamentos
+    {
+
+        public int DepartamentoID { get; set; }
+
+        public String Nome { get; set; }
+    }
+}
